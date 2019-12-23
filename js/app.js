14859b1cdd75473d75df4f73c45866c6c19fbe90
Eldar Bajric <eldar.bajric96@gmail.com>
2019-12-23 09:58:23 +0100

    Added functions to delete item. Updated all controlers so that item is deleted from all of them.

--- js/app.js
+++ js/app.js
@@ -82,6 +82,21 @@ var budgetController = (function () {
             
         },
 
+        deleteItem: function (type, id) {
+            var ids, index;
+            
+            ids = data.allItems[type].map(function(current){
+                return current.id;
+            });
+            
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+
+        },
+
         calculateBudget: function() {
             // Calculate total income and expenses
                 calculateTotal('exp');
@@ -145,7 +160,9 @@ var UIController = (function () {
         incomeLabel: '.budget__income--value',
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        container: '.container',
+        dateLabel: '.budget__title--month'
 
     };
 
@@ -172,11 +189,11 @@ var UIController = (function () {
 
             if(type === 'inc'){
                 element = DOMstrings.incomeContainer;
-                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
 
             }else if (type = 'exp') {
                 element = DOMstrings.expensesContainer;
-                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
 
             }
 
@@ -193,6 +210,12 @@ var UIController = (function () {
 
 
         },
+
+        deleteListItem: function (selectorID) {
+            var element = document.getElementById(selectorID);
+            element.parentNode.removeChild(element);
+        },
+
 
         // Clear input fields after input has been made by user
         clearFields: function() {
@@ -237,6 +260,19 @@ var UIController = (function () {
 
 
             },
+
+            displayMonth: function () {
+                var now, year, month, months;
+
+                now = new Date();
+
+
+
+                year = now.getFullYear();
+                month = now.getUTCMonth();
+                months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+                document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+            },
         
 
         getDOMstrings: function () {
@@ -262,6 +298,8 @@ var controller = (function (budgetCtrl, UICtrl) {
                 ctrlAddItem();
             }
         });
+
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
     };
           
     var updateBudget = function() {
@@ -317,9 +355,33 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     };
 
+    var ctrlDeleteItem = function(event) {
+        var itemID, splitID, type, ID;
+
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        
+        if(itemID) {
+
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = parseInt(splitID[1]);
+
+            // 1. Delete item from the data structure
+            budgetCtrl.deleteItem(type, ID);
+            // 2. Delete the item from th UI
+            UICtrl.deleteListItem(itemID);
+            // 3. Update and show the budget
+            updateBudget();
+
+
+        }
+
+    };
+
     return {
         init: function () {
             console.log('Application has started')
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
